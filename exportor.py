57e3e6b8a287d930d4cc587d1fc80570c9e38bf5
Shi.Zongjie <shizongjie@gmail.com>
2023-12-18 22:45:08 +0800

    Add Error Tolerance

--- exportor.py
+++ exportor.py
@@ -32,83 +32,86 @@ if __name__ == '__main__':
         serverTimePercentile.clear()
         serverTimeStats.clear()
         
-        query = datasource.getMetrics()
-        
-        for item in query['aggregations']['hostName']['buckets']:
-            hostName = item['key']
-            totalHits = item['doc_count']
-
-            for region in item['region']['buckets']:
-                country = region['key']
-                regionHits = region['doc_count']
-
-                for hitStatus in region['cacheStatus']['buckets']:
-                    key = int(hitStatus['key'])
-                    if key == 1:
-                        hitCount = hitStatus['doc_count']
-                    
-                    if key == 0:
-                        missCount = hitStatus['doc_count']
-
-                #hitRatio = round(hitCount / regionHits, 4)
-
-                httpRequestCount.labels(hostName, country, "requests").set(regionHits)
-                httpRequestCount.labels(hostName, country, "hit").set(hitCount)
-                httpRequestCount.labels(hostName, country, "miss").set(missCount)
-                #print(hostName, country, regionHits, hitCount, missCount)
-                
-                for httpCode in region['requestCount']['buckets']:
-                    code = httpCode['key']
-                    count = httpCode['doc_count']
-
-                    httpCodeStatus.labels(hostName, country, code).set(count)
-                    #print(httpCode['key'], httpCode['doc_count'])
-
-
-                for suffix in ['avg', 'min', 'max', 'std_deviation', 'variance']:
-                    responseTime = region['stats_responseTime'][suffix]
-                    serverTimeStats.labels(hostName, country, "responseTime", suffix).set(responseTime)
-
-                    rspContentLen = region['stats_rspContentLen'][suffix]
-                    serverTimeStats.labels(hostName, country, "contentLength", suffix).set(rspContentLen)
-
-                    tlsOverheadTime = region['stats_tlsOverheadTime'][suffix]
-                    serverTimeStats.labels(hostName, country, "tlsOverheadTime", suffix).set(tlsOverheadTime)
-
-                    transferTime = region['stats_transferTime'][suffix]
-                    serverTimeStats.labels(hostName, country, "transferTime", suffix).set(transferTime)
-
-                    reqEndTime = region['stats_reqEndTime'][suffix]
-                    serverTimeStats.labels(hostName, country, "reqEndTime", suffix).set(reqEndTime)
-
-                    turnAroundTime = region['stats_turnAroundTime'][suffix]
-                    serverTimeStats.labels(hostName, country, "turnAroundTime", suffix).set(turnAroundTime)
-
-                
-                for percent in ['5.0', '10.0', '50.0', '90.0', '95.0', '99.0']:
-                    percentile = 'p' + percent.replace('.0', '')
-
-                    responseTime = region['responseTime']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "responseTime", percentile).set(responseTime)
-
-                    rspContentLen = region['rspContentLen']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "contentLength", percentile).set(responseTime)
-
-                    transferTime = region['transferTime']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "transferTime", percentile).set(transferTime)
-
-                    turnAroundTime = region['turnAroundTime']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "turnAroundTime", percentile).set(responseTime)
-
-                    tlsOverheadTime = region['tlsOverheadTime']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "tlsOverheadTime", percentile).set(responseTime)
-
-                    reqEndTime = region['reqEndTime']['values'][percent]
-                    serverTimePercentile.labels(hostName, country, "reqEndTime", percentile).set(responseTime)
-
-
+        try:
+            query = datasource.getMetrics()
             
+            for item in query['aggregations']['hostName']['buckets']:
+                hostName = item['key']
+                totalHits = item['doc_count']
 
+                for region in item['region']['buckets']:
+                    country = region['key']
+                    regionHits = region['doc_count']
+
+                    for hitStatus in region['cacheStatus']['buckets']:
+                        key = int(hitStatus['key'])
+                        if key == 1:
+                            hitCount = hitStatus['doc_count']
+                        
+                        if key == 0:
+                            missCount = hitStatus['doc_count']
+
+                    #hitRatio = round(hitCount / regionHits, 4)
+
+                    httpRequestCount.labels(hostName, country, "requests").set(regionHits)
+                    httpRequestCount.labels(hostName, country, "hit").set(hitCount)
+                    httpRequestCount.labels(hostName, country, "miss").set(missCount)
+                    #print(hostName, country, regionHits, hitCount, missCount)
+                    
+                    for httpCode in region['requestCount']['buckets']:
+                        code = httpCode['key']
+                        count = httpCode['doc_count']
+
+                        httpCodeStatus.labels(hostName, country, code).set(count)
+                        #print(httpCode['key'], httpCode['doc_count'])
+
+
+                    for suffix in ['avg', 'min', 'max', 'std_deviation', 'variance']:
+                        responseTime = region['stats_responseTime'][suffix]
+                        serverTimeStats.labels(hostName, country, "responseTime", suffix).set(responseTime)
+
+                        rspContentLen = region['stats_rspContentLen'][suffix]
+                        serverTimeStats.labels(hostName, country, "contentLength", suffix).set(rspContentLen)
+
+                        tlsOverheadTime = region['stats_tlsOverheadTime'][suffix]
+                        serverTimeStats.labels(hostName, country, "tlsOverheadTime", suffix).set(tlsOverheadTime)
+
+                        transferTime = region['stats_transferTime'][suffix]
+                        serverTimeStats.labels(hostName, country, "transferTime", suffix).set(transferTime)
+
+                        reqEndTime = region['stats_reqEndTime'][suffix]
+                        serverTimeStats.labels(hostName, country, "reqEndTime", suffix).set(reqEndTime)
+
+                        turnAroundTime = region['stats_turnAroundTime'][suffix]
+                        serverTimeStats.labels(hostName, country, "turnAroundTime", suffix).set(turnAroundTime)
+
+                    
+                    for percent in ['5.0', '10.0', '50.0', '90.0', '95.0', '99.0']:
+                        percentile = 'p' + percent.replace('.0', '')
+
+                        responseTime = region['responseTime']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "responseTime", percentile).set(responseTime)
+
+                        rspContentLen = region['rspContentLen']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "contentLength", percentile).set(responseTime)
+
+                        transferTime = region['transferTime']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "transferTime", percentile).set(transferTime)
+
+                        turnAroundTime = region['turnAroundTime']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "turnAroundTime", percentile).set(responseTime)
+
+                        tlsOverheadTime = region['tlsOverheadTime']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "tlsOverheadTime", percentile).set(responseTime)
+
+                        reqEndTime = region['reqEndTime']['values'][percent]
+                        serverTimePercentile.labels(hostName, country, "reqEndTime", percentile).set(responseTime)
+
+
+        except:
+            time.sleep(5)
+            continue
+        
         time.sleep(10)
 
             
